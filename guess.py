3d4b350ba696cd49f1eff732aef39bd6f3f1c8d5
Garoto0518 <gabrielrguez0518@gmail.com>
2017-08-06 17:43:29 -0400

    First commit, Simple guessing game

---
+++ guess.py
@@ -0,0 +1,29 @@
+# This is a guess the number game.
+import random
+guessesTaken = 0
+
+print('Whatsabiiiii! What is your name/ Como te llamas?')
+myName = input("")
+
+x = random.randint(1, 100)
+print('Interesting, ' + myName + ', Garoto as they call me is thinking of a number between 1 and 100. Can you guess what the number?')
+
+while guessesTaken < 10:
+    # print('Take a guess / Adivina el numero ') # There are four spaces in front of print.
+    y = int(input("Guess a number? / Adivina el numero. "))
+    guessesTaken = guessesTaken + 1
+    if y < x:
+        print('Your guess is too low. /  Su respuesta es muy bajita ') # There are eight spaces in front of print.
+        print("You have / Tienes " + str(10 - guessesTaken) + " chance left / turnos restantes")
+    elif y > x:
+        print('Your guess is too high. / Su respuesta es muy alta ')
+        print("You have / Tienes " + str(10 - guessesTaken) + " chance left / turnos restantes")
+    elif y == x:
+        print("You Win / Ganaste")
+        guessesTaken = str(guessesTaken)
+        print('Good job, / Bien Hecho' + myName + '! You guessed my number in / Adivinaste mi numero' + guessesTaken + ' guesses!/ respuestas')
+        break
+
+if y != x:
+    x = str(x)
+    print('Nope. The number I was thinking of was/ No. El numero que estaba pensando era ' + x)
